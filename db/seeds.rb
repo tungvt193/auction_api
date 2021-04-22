b56cc3a6d2a63c3a0182fae9ffe34f9e11d3822e
John Nguyễn <john@techfox.io>
2021-04-22 22:48:40 +0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.delete_all
-# Category.delete_all
-# Banner.delete_all
-# Company.delete_all
+User.delete_all
+Category.delete_all
+Banner.delete_all
+Company.delete_all
 ProductCompany.delete_all
 Product.delete_all
 
-# load Rails.root.join('db', 'seeds', 'user_sd.rb')
-# load Rails.root.join('db', 'seeds', 'category_sd.rb')
-# load Rails.root.join('db', 'seeds', 'banner_sd.rb')
-# load Rails.root.join('db', 'seeds', 'company_sd.rb')
+load Rails.root.join('db', 'seeds', 'user_sd.rb')
+load Rails.root.join('db', 'seeds', 'category_sd.rb')
+load Rails.root.join('db', 'seeds', 'banner_sd.rb')
+load Rails.root.join('db', 'seeds', 'company_sd.rb')
 load Rails.root.join('db', 'seeds', 'product_sd.rb')
