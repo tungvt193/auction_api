c0e28d0d3dc0161dd62e8fb91fcff9d927a4199b
John Nguyễn <john@techfox.io>
2021-05-10 15:58:41 +0700

    reset message authorized

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -19,13 +19,13 @@ module Api
     def detect_current_user
       token = decode_access_token
 
-      raise ActionController::InvalidAuthenticityToken, 'Token bạn gửi đi không hợp lệ. Vui lòng đăng nhập lại!' if token.blank?
+      raise ActionController::InvalidAuthenticityToken, 'Phiên đăng nhập không hợp lệ. Vui lòng thử lại!' if token.blank?
 
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
       decoded_token = crypt.decrypt_and_verify token
       items = decoded_token.split('&')
 
-      raise ActionController::InvalidAuthenticityToken, 'Token bạn gửi đi không hợp lệ. Vui lòng đăng nhập lại!' if items&.first.blank?
+      raise ActionController::InvalidAuthenticityToken, 'Phiên đăng nhập không hợp lệ. Vui lòng thử lại!' if items&.first.blank?
 
       user_id = items[0].gsub('user-id:', '').to_i
       expired_at = items[1]&.gsub('expired-at:', '')
@@ -33,11 +33,11 @@ module Api
 
       user = User.find_by(id: user_id)
 
-      raise ActionController::InvalidAuthenticityToken, 'Token bạn gửi đi không hợp lệ. Hãy tạo lại yêu cầu thay đổi mật khẩu!' if token_is_invalid(token, token_type, user)
+      raise ActionController::InvalidAuthenticityToken, 'Phiên đăng nhập không hợp lệ. Hãy tạo lại yêu cầu thay đổi mật khẩu!' if token_is_invalid(token, token_type, user)
 
       return user if token_type == 'crawler'
 
-      raise ActionController::InvalidAuthenticityToken, 'Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại!' if expired_at.blank? || Time.zone.parse(expired_at) < Time.zone.now
+      raise ActionController::InvalidAuthenticityToken, 'Phiên đăng nhập đã hết hạn. Vui lòng thử lại!' if expired_at.blank? || Time.zone.parse(expired_at) < Time.zone.now
 
       user
     end
