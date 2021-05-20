a57bf5d1c4a85378fbcd580cda3b2b1c63da3709
John Nguyễn <john@techfox.io>
2021-05-20 15:25:07 +0700

    fix bug

--- app/graphql/policies/admin_policy.rb
+++ app/graphql/policies/admin_policy.rb
@@ -3,9 +3,12 @@ module Policies
     class << self
       def query
         {
-          '*': {
-            guard: ->(_obj, _args, ctx) { is_admin?(ctx) }
-          }
+          'v1AdminBookingSummary': admin_guard,
+          'v1AdminBookingList': admin_guard,
+          'v1AdminBooking': admin_guard,
+          'v1AdminNotificationsList': admin_guard,
+          'v1AdminNotifications': admin_guard,
+          'v1AdminCustomers': admin_guard
         }
       end
 
@@ -24,6 +27,11 @@ module Policies
       end
 
       private
+
+
+      def admin_guard
+        { guard: ->(_obj, _args, ctx) { is_admin?(ctx) } }
+      end
 
       def is_admin?(ctx)
         ctx[:current_user].try(:admin?)
