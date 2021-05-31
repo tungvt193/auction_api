09b246083c688d4bca8cdc5db27c24b0b3bf722d
John Nguyễn <john@techfox.io>
2021-05-31 11:40:50 +0700

    add published_at for video and news

--- app/graphql/resolvers/common/news/show.rb
+++ app/graphql/resolvers/common/news/show.rb
@@ -6,7 +6,7 @@ module Resolvers
         type ::Types::NewsType, null: false
 
         def resolve(id:)
-          news = ::News.where(id_or_slug: try_decode(id)).first
+          news = ::News.graphql_ransack(id_or_slug_eq: try_decode(id)).first
           raise ActiveRecord::RecordNotFound, 'Không tìm thấy tài bài viết này!' if news.blank?
 
           news
--- app/graphql/resolvers/common/videos/show.rb
+++ app/graphql/resolvers/common/videos/show.rb
@@ -6,7 +6,7 @@ module Resolvers
         type ::Types::VideoType, null: false
 
         def resolve(id:)
-          video = ::Video.where(id_or_slug: try_decode(id)).first
+          video = ::Video.graphql_ransack(id_or_slug_eq: try_decode(id)).first
           raise ActiveRecord::RecordNotFound, 'Không tìm thấy tài video này!' if video.blank?
 
           video
