1c3b279da7127ff6133bb259667733a5ecc429b1
John Nguyễn <john@techfox.io>
2021-06-03 15:57:44 +0700

    fix rubocop

--- db/seeds/administrative_division_sd.rb
+++ db/seeds/administrative_division_sd.rb
@@ -4,36 +4,35 @@ json_data = File.read(Rails.root.join('db/jsons/hanhchinh-vietnam.json'))
 provinces = JSON.parse(json_data)
 now = Time.zone.now
 
-
 province_attributes = []
 district_attributes = []
 ward_attributes = []
 
 provinces.each do |province|
   province_attributes.push({
-    id: province['Id'].to_i,
-    name: province['Name'],
-    created_at: now,
-    updated_at: now
-  })
+                             id: province['Id'].to_i,
+                             name: province['Name'],
+                             created_at: now,
+                             updated_at: now
+                           })
 
   province['Districts'].each do |district|
     district_attributes.push({
-      id: district['Id'].to_i,
-      name: district['Name'],
-      province_id: province['Id'].to_i,
-      created_at: now,
-      updated_at: now
-    })
+                               id: district['Id'].to_i,
+                               name: district['Name'],
+                               province_id: province['Id'].to_i,
+                               created_at: now,
+                               updated_at: now
+                             })
 
     district['Wards'].each do |ward|
       ward_attributes.push({
-        id: ward['Id'].to_i,
-        name: ward['Name'],
-        district_id: district['Id'].to_i,
-        created_at: now,
-        updated_at: now 
-      })
+                             id: ward['Id'].to_i,
+                             name: ward['Name'],
+                             district_id: district['Id'].to_i,
+                             created_at: now,
+                             updated_at: now
+                           })
     end
   end
 end
