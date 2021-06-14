f3f347d387e3e530b32deb82e4f52db1909c7dc6
John Nguyễn <john@techfox.io>
2021-06-14 09:38:51 +0700

    fix booking_namme

--- app/graphql/lazy/lazy_booking_name.rb
+++ app/graphql/lazy/lazy_booking_name.rb
@@ -1,11 +1,15 @@
 module Lazy
   class LazyBookingName < Base
     def call(obj, _args, _ctx)
-      booking_name(obj.try(:auction_item))
+      BatchLoader::GraphQL.for(obj.try(:auction_item_id)).batch(default_value: nil, cache: false) do |ids, loader|
+        ::AuctionItem.joins(:company).where(id: ids).select(:id, :auction_name, :serial, 'companies.name as company_name').each do |aum|
+          loader.call(aum.try(:id)) { booking_name(aum) }
+        end
+      end
     end
 
-    def booking_name(auction_item)
-      "#{auction_item.company.name} #{auction_item.auction_name}-#{auction_item.serial}"
+    def booking_name(aum)
+      "#{aum.try(:company_name)} #{aum.auction_name}-#{aum.serial}"
     end
   end
 end
