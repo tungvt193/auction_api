6e5a1c7236e1c781e4db9c4ac48d87369f541bda
John Nguyễn <john@techfox.io>
2021-06-21 17:20:26 +0700

    api image

--- app/graphql/mutations/admin/images/create_image.rb
+++ app/graphql/mutations/admin/images/create_image.rb
@@ -3,6 +3,7 @@ module Mutations
     module Images
       class CreateImage < BaseMutation
         argument :file, Types::FileType, required: true
+        argument :attribute, Types::AttributeType, required: true
         field :data, ::Types::ImageType, null: false
 
         def resolve(args)
@@ -11,11 +12,18 @@ module Mutations
           resource = collection.new
 
           ApplicationRecord.transaction do
+            resource.assign_attributes(normalize_parameters)
             resource.file = params[:file]
             resource.save!
           end
 
           { data: resource }
+        end
+        
+        private
+
+        def normalize_params
+          params.require(:attribute).permit(:imageable_type, :imageable_id)
         end
       end
     end
