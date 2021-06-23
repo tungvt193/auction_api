75e27e0c8290afc42e679595af00a640cc2b9f96
John Nguyễn <john@techfox.io>
2021-06-23 14:04:21 +0700

    fix api estimated_price

--- app/graphql/resolvers/common/auction_items/estimated_price.rb
+++ app/graphql/resolvers/common/auction_items/estimated_price.rb
@@ -3,14 +3,21 @@ module Resolvers
     module AuctionItems
       class EstimatedPrice < ::Resolvers::BaseResolver
         argument :price, Float, required: true
+        argument :unit, String, required: false
         argument :id, ID, required: true
         type ::Types::EstimatedPriceType, null: false
 
-        def resolve(id:, price:)
-          aui = super
+        def resolve(args)
+          aui = super(id: args[:id])
           product = aui.try(:product)
 
-          Estimated.estimate(product, price)
+          ::Estimated.estimate(product, args[:price], args[:unit])
+        end
+
+        private
+
+        def model
+          '::AuctionItem'
         end
       end
     end
