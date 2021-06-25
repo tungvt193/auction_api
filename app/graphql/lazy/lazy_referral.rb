08e8c77526afedb5c20b7b36527d0ee4d4a05c2a
John Nguyễn <john@techfox.io>
2021-06-25 11:28:38 +0700

    fix ui

--- app/graphql/lazy/lazy_referral.rb
+++ app/graphql/lazy/lazy_referral.rb
@@ -1,5 +1,5 @@
 module Lazy
-  class LazyFerral < Base
+  class LazyReferral < Base
     def call(obj, _args, _ctx)
       BatchLoader::GraphQL.for(obj.try(:referral_id)).batch(default_value: nil, cache: false) do |ids, loader|
         ::User.where(id: ids).each do |user|
