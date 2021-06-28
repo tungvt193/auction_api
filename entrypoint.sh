efc0b563222b89165e20e18fbd713ad70fee45a2
John Nguyễn <john@techfox.io>
2021-06-28 13:23:20 +0700

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /auction-api/tmp/pids/server.pid
 
-RAILS_ENV=production bundle exec rails db:create
-RAILS_ENV=production bundle exec rails migration:apply
+bundle exec rails db:create
+bundle exec rails migration:apply
 bundle exec rails s -p 3000 -b '0.0.0.0'
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
