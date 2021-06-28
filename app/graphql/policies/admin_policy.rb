436fa161a146d16165c3653ddb2aacdcee537bd1
John Nguyễn <john@techfox.io>
2021-06-28 14:55:09 +0700

    fix rubocop

--- app/graphql/policies/admin_policy.rb
+++ app/graphql/policies/admin_policy.rb
@@ -12,7 +12,7 @@ module Policies
           'v1AdminNewsList': admin_guard,
           'v1AdminNews': admin_guard,
           'v1AdminVideoList': admin_guard,
-          'v1AdminVideo': admin_guard,
+          'v1AdminVideo': admin_guard
         }
       end
 
