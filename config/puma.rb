f6508c2e09cd1947cd24887c4278e89c57aef844
John Nguyễn <john@techfox.io>
2021-06-28 13:10:15 +0700

    fix gitlab-ci

--- config/puma.rb
+++ config/puma.rb
@@ -1,38 +1,43 @@
-#!/usr/bin/env puma
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
-environment ENV['RAILS_ENV']
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+#
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-return unless %w[production staging].include?(ENV['RAILS_ENV'])
+# Specifies the `port` that Puma will listen on to receive requests; default is 4040.
+#
+port ENV.fetch("PORT") { 4040 }
 
-directory '/home/root/apps/auction/current'
-rackup '/home/root/apps/auction/current/config.ru'
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-tag ''
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-pidfile '/home/root/apps/auction/shared/tmp/pids/puma.pid'
-state_path '/home/root/apps/auction/shared/tmp/pids/puma.state'
-stdout_redirect '/home/root/apps/auction/shared/log/puma_access.log', '/home/root/apps/auction/shared/log/puma_error.log', true
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked web server processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-threads 4, 16
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+# preload_app!
 
-bind 'unix:///home/root/apps/auction/shared/tmp/sockets/auction-puma.sock'
-
-workers 0
-
-preload_app!
-
-on_restart do
-  Rails.logger.info('Refreshing Gemfile')
-
-  ENV['BUNDLE_GEMFILE'] = ''
-end
-
-before_fork do
-  ActiveRecord::Base.connection_pool.disconnect!
-end
-
-on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
-end
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
