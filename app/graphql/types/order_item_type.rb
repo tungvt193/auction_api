c600dd21f50d7258bfb9c64deb6913af7152480f
John Nguyễn <john@techfox.io>
2021-07-01 14:13:36 +0700

    fix bug

--- app/graphql/types/order_item_type.rb
+++ app/graphql/types/order_item_type.rb
@@ -32,7 +32,6 @@ module Types
     field :auction_item_id, ID, null: false
     field :auction_id, ID, null: false
     field :product_id, ID, null: false
-    field :payment_type, String, null: true
     field :price, Float, null: true
     field :quantity, Int, null: true
     field :jpy_exrate, Float, null: true, default_value: 0.0
