6ccaaa2d9f23cc04b1c871136448cd835a07855c
John Nguyễn <john@techfox.io>
2021-07-27 10:40:01 +0700

    fix bug

--- app/graphql/mutations/common/bookings/update_booking.rb
+++ app/graphql/mutations/common/bookings/update_booking.rb
@@ -25,7 +25,7 @@ module Mutations
         private
 
         def normalize_parameters
-          params.permit(
+          params.require(:attribute).permit(
             :auction_item_id, :booking_type, :address,
             :zoom_id, :zoom_password, :supporter_id, :booking_at,
             :deposit_type, :payment_type, :price, :status
